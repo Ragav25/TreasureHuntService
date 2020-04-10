879fe4871a27fa381e9dc8d71844a23c6f6e7a26
Ragav25 <ragavravi25@gmail.com>
2020-04-10 15:29:49 +0100

    Inital setup added

---
+++ src/main/java/com/krr/project/treausrehunt/TreausrehuntApplication.java
@@ -0,0 +1,13 @@
+package com.krr.project.treausrehunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreausrehuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreausrehuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krr/project/treausrehunt/TreausrehuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krr.project.treausrehunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreausrehuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
